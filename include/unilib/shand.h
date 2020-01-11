275b54252aecd28f8e92897d64f26ce5c99a28d6
Alexander Nicholi <me@nicholatian.com>
2020-01-11 02:14:59 -0500

    update targeting directions

--- include/unilib/shand.h
+++ include/unilib/shand.h
@@ -26,7 +26,6 @@ typedef unsigned __int64 ptri;
 #endif /* _M_ arch */
 /* UNIX land */
 #elif defined( __clang__ ) || defined( __GNUC__ )
-typedef _Float16 f16; /* this is unsupported on ARM because of ABIs */
 typedef __INT64_TYPE__ s64;
 typedef __INT32_TYPE__ s32;
 typedef __INT16_TYPE__ s16;
@@ -36,6 +35,17 @@ typedef __UINT32_TYPE__ u32;
 typedef __UINT16_TYPE__ u16;
 typedef __UINT8_TYPE__ u8;
 typedef __UINTPTR_TYPE__ ptri;
+#endif
+
+typedef long double f80;
+typedef double f64;
+typedef float f32;
+
+#if defined( __clang__) && (defined( __arm__) || defined( __aarch64__ ))
+typedef _Float16 f16;
+#else
+/* this is unsupported outside ARM because of ABIs */
+typedef f32 f16;
 #endif
 
 #endif /* INC__UNILIB_SHAND_H */
