a123d4f217c56995ae5a0056fd68bc4a146a5c52
Alexander Nicholi <me@nicholatian.com>
2020-01-30 17:34:21 -0500

    test battery skeleton

---
+++ test/main.c
@@ -0,0 +1,20 @@
+/****************************************************************************\
+ *                           Universal C Library™                           *
+ *                                                                          *
+ *                      Copyright © 2019 ARQADIUM, LLC                      *
+ *                           All rights reserved.                           *
+\****************************************************************************/
+
+#include <tes/battery.h>
+
+extern void tes_pre( int ac, char* av[] )
+{
+}
+
+extern int tes_tests( void )
+{
+}
+
+extern void tes_post( void )
+{
+}
