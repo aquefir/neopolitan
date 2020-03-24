44aa42af4d56b5498cb7fc5f2a5ccefb1b5c8f8c
Alexander Nicholi <me@nicholatian.com>
2020-03-23 23:35:40 -0400

    implement UNI_DEPRECATED

--- include/uni/decl.h
+++ include/uni/decl.h
@@ -1,7 +1,7 @@
 /****************************************************************************\
- *                           Universal C Library™                           *
+ *                           Aquefir One Library™                           *
  *                                                                          *
- *                    Copyright © 2019-2020 ARQADIUM LLC                    *
+ *                      Copyright © 2019-2020 Aquefir                       *
  *                       Released under BSD-2-Clause.                       *
 \****************************************************************************/
 
@@ -12,6 +12,15 @@
 #define UNI_C_API extern "C"
 #else
 #define UNI_C_API
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+#define UNI_DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define UNI_DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement UNI_DEPRECATED for this compiler")
+#define UNI_DEPRECATED
 #endif
 
 #endif /* INC_API__UNI_DECL_H */
