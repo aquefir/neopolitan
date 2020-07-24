930dad8f3e0e28511c3492e737549f9e3262de2d
Alexander Nicholi <me@nicholatian.com>
2020-07-24 06:40:50 -0400

    add uni_strequ() to compare strings, remove a few sigs, implement several char functions

--- src/str.c
+++ src/str.c
@@ -7,6 +7,7 @@
 
 #include "str.h"
 
+#include <uni/err.h>
 #include <uni/memory.h>
 
 #include "utf8.h"
@@ -17,7 +18,7 @@ ptri uni_strlen( const char* in )
 
 	if( in == NULL )
 	{
-		return 0;
+		uni_die( );
 	}
 
 	for( i = 0; in[i] != '\0'; ++i )
@@ -30,55 +31,86 @@ ptri uni_strlencp( const char* in )
 {
 	ptri i = 0;
 
+	if(!in)
+	{
+		uni_die( );
+	}
+
 	return _uni_validcountu8( in, &i ) ? i : 0;
 }
 
 char* uni_strdup( const char* in )
 {
-	char* ret;
-	const ptri sz = uni_strlen( in );
+	if(!in)
+	{
+		uni_die( );
+	}
 
-	ret = uni_alloc( sz + 1 );
+	{
+		char* ret;
+		const ptri sz = uni_strlen( in );
 
-	uni_memcpy( ret, in, sz );
-	ret[sz] = '\0';
+		ret = uni_alloc( sz + 1 );
 
-	return ret;
+		uni_memcpy( ret, in, sz );
+		ret[sz] = '\0';
+
+		return ret;
+	}
 }
 
 char* uni_strndup( const char* in, ptri len )
 {
-	char* ret;
-	const ptri sz = uni_strlen( in );
-	const ptri amt = sz > len ? len : sz;
+	if(!in || !len)
+	{
+		uni_die( );
+	}
 
-	ret = uni_alloc( amt + 1 );
+	{
+		char* ret;
+		const ptri sz = uni_strlen( in );
+		const ptri amt = sz > len ? len : sz;
 
-	uni_memcpy( ret, in, amt );
-	ret[amt] = '\0';
+		ret = uni_alloc( amt + 1 );
 
-	return ret;
+		uni_memcpy( ret, in, amt );
+		ret[amt] = '\0';
+
+		return ret;
+	}
 }
 
 char** uni_strdupv( char** in )
 {
-	char** ret;
-	ptri i;
-	const ptri arrsz = uni_strlenv( in );
-
-	ret = uni_alloc0( sizeof(char*) * (arrsz + 1 ) );
-
-	for(i = 0; i < arrsz; ++i)
+	if(!in)
 	{
-		ret[i] = uni_strdup( in[i] );
+		uni_die( );
 	}
 
-	return ret;
+	{
+		char** ret;
+		ptri i;
+		const ptri arrsz = uni_strlenv( in );
+
+		ret = uni_alloc0( sizeof(char*) * (arrsz + 1 ) );
+
+		for(i = 0; i < arrsz; ++i)
+		{
+			ret[i] = uni_strdup( in[i] );
+		}
+
+		return ret;
+	}
 }
 
 char* uni_strnfill( ptri sz, char c )
 {
 	char* ret;
+
+	if(!sz)
+	{
+		uni_die( );
+	}
 
 	ret = uni_alloc( sz + 1 );
 
@@ -90,62 +122,331 @@ char* uni_strnfill( ptri sz, char c )
 
 char* uni_strstr( const char* nee, const char* hay )
 {
-	ptri i;
-	const ptri nee_sz = uni_strlen( nee );
-	const ptri hay_sz = uni_strlen( hay );
-	const ptri hay_limit = hay_sz - nee_sz;
-
-	for(i = 0; i < hay_limit; ++i)
+	if(!nee || !hay)
 	{
-		ptri j;
-		int match;
+		uni_die( );
+	}
 
-		match = 1;
+	{
+		ptri i;
+		const ptri nee_sz = uni_strlen( nee );
+		const ptri hay_sz = uni_strlen( hay );
+		const ptri hay_limit = hay_sz - nee_sz;
 
-		for(j = 0; j < nee_sz; ++j)
+		for(i = 0; i < hay_limit; ++i)
 		{
-			if(hay[i + j] != nee[j])
-			{
-				match = 0;
+			ptri j;
+			int match;
 
-				break;
+			match = 1;
+
+			for(j = 0; j < nee_sz; ++j)
+			{
+				if(hay[i + j] != nee[j])
+				{
+					match = 0;
+
+					break;
+				}
+			}
+
+			if(match)
+			{
+				return hay + i;
 			}
 		}
 
-		if(match)
-		{
-			return (char*)((u8*)hay + i);
-		}
+		return NULL;
 	}
 }
 
 char* uni_strrstr( const char* nee, const char* hay )
 {
-	ptri i;
-	const ptri nee_sz = uni_strlen( nee );
-	const ptri hay_sz = uni_strlen( hay );
-	const ptri hay_limit = hay_sz - nee_sz;
-
-	for(i = hay_limit - 1; i >= 0; --i)
+	if(!nee || !hay)
 	{
-		ptri j;
-		int match;
+		uni_die( );
+	}
 
-		match = 1;
+	{
+		ptri i;
+		const ptri nee_sz = uni_strlen( nee );
+		const ptri hay_sz = uni_strlen( hay );
+		const ptri hay_limit = hay_sz - nee_sz;
+		char* ret;
 
-		for(j = nee_sz - 1; j >= 0; --j)
+		for(i = 0; i < hay_limit; ++i)
 		{
-			if(hay[i - j] != nee[j])
-			{
-				match = 0;
+			ptri j;
+			int match;
 
+			match = 1;
+
+			for(j = 0; j < nee_sz; ++j)
+			{
+				if(hay[i + j] != nee[j])
+				{
+					match = 0;
+
+					break;
+				}
+			}
+
+			if(match)
+			{
+				ret = hay + i;
+			}
+		}
+
+		return ret;
+	}
+}
+
+int uni_strequ( const char* a, const char* b )
+{
+	if(!a || !b)
+	{
+		uni_die( );
+	}
+
+	{
+		ptri i;
+		const ptri a_sz = uni_strlen( a );
+		const ptri b_sz = uni_strlen( b );
+
+		if(b_sz != a_sz)
+		{
+			return 0;
+		}
+
+		for(i = 0; i < b_sz; ++i)
+		{
+			if(a[i] != b[i])
+			{
+				return 0;
+			}
+		}
+
+		return 1;
+	}
+}
+
+int uni_strpre( const char* a, const char* b )
+{
+	if(!a || !b)
+	{
+		uni_die( );
+	}
+
+	{
+		ptri i;
+		const ptri a_sz = uni_strlen( a );
+		const ptri b_sz = uni_strlen( b );
+
+		if(b_sz > a_sz)
+		{
+			return 0;
+		}
+
+		for(i = 0; i < b_sz; ++i)
+		{
+			if(a[i] != b[i])
+			{
+				return 0;
+			}
+		}
+
+		return 1;
+	}
+}
+
+int uni_strsuf( const char* a, const char* b )
+{
+	if(!a || !b)
+	{
+		uni_die( );
+	}
+
+	{
+		ptri i;
+		const ptri a_sz = uni_strlen( a );
+		const ptri b_sz = uni_strlen( b );
+
+		if(b_sz > a_sz)
+		{
+			return 0;
+		}
+
+		for(i = a_sz - b_sz; i < a_sz; ++i)
+		{
+			if(a[i] != b[i])
+			{
+				return 0;
+			}
+		}
+
+		return 1;
+	}
+}
+
+int uni_isascii( const char* in )
+{
+	if(!in)
+	{
+		uni_die( );
+	}
+
+	{
+		ptri i;
+		const ptri in_sz = uni_strlen( in );
+
+		for(i = 0; i < in_sz; ++i)
+		{
+			if((u8)(in[i]) >= 0x80)
+			{
+				return 0;
+			}
+		}
+
+		return 1;
+	}
+}
+
+int uni_isalnum( char c )
+{
+	return uni_isalpha( c ) || uni_isdigit( c );
+}
+
+int uni_isalpha( char c )
+{
+	return uni_isupper( c ) || uni_islower( c );
+}
+
+int uni_isdigit( char c )
+{
+	return (u8)c >= 0x30 && (u8)c <= 0x39;
+}
+
+int uni_isxdigit( char c )
+{
+	return ((u8)c >= 0x30 && (u8)c <= 0x39) || \
+		((u8)c >= 0x41 && (u8)c <= 0x46) || \
+		((u8)c >= 0x61 && (u8)c <= 0x66);
+}
+
+int uni_iscntrl( char c )
+{
+	return ((u8)c >= 0x01 && (u8)c <= 0x1F) || (u8)c == 0x7F;
+}
+
+int uni_isprint( char c )
+{
+	return (u8)c >= 0x20 && (u8)c <= 0x7E;
+}
+
+int uni_ispunct( char c )
+{
+	const u8 ch = (u8)c;
+
+	return (ch >= 0x21 && ch <= 0x2F) || (ch >= 0x3A && ch <= 0x40) || \
+		(ch >= 0x5B && ch <= 0x60) || (ch >= 0x7B && ch <= 0x7E);
+}
+
+int uni_isspace( char c )
+{
+	return (u8)c == 0x20 || ((u8)c >= 0x09 && (u8)c <= 0x0D);
+}
+
+int uni_isupper( char c )
+{
+	return (u8)c >= 0x41 && (u8)c <= 0x5A;
+}
+
+int uni_isupper( char c )
+{
+	return (u8)c >= 0x61 && (u8)c <= 0x7A;
+}
+
+int uni_digitval( char c )
+{
+	if((u8)c < 0x30)
+	{
+		return 0;
+	}
+
+	return (u8)c - 0x30;
+}
+
+int uni_xdigitval( char c )
+{
+	/* decimal digit, or out of bounds. digitval will handle it */
+	if((u8)c <= 0x39)
+	{
+		return uni_digitval( c );
+	}
+	/* lowercase alpha */
+	else if((u8)c >= 0x61)
+	{
+		return (u8)c - 0x57;
+	}
+
+	/* else: uppercase alpha */
+	return (u8)c - 0x37;
+}
+
+char* uni_strchomp( char* in, char c )
+{
+	if(!in || !c)
+	{
+		uni_die( );
+	}
+
+	{
+		char* ret;
+		ptri i, offs;
+		const ptri in_sz = uni_strlen( in );
+
+		for(i = 0; i < in_sz; ++i)
+		{
+			if(in[i] != c)
+			{
 				break;
 			}
 		}
 
-		if(match)
+		offs = i;
+
+		for(i = offs; i < in_sz; ++i)
 		{
-			return (char*)((u8*)hay + i);
+			in[i - offs] = in[i];
 		}
+
+		in[in_sz - offs] = '\0';
+
+		return in;
+	}
+}
+
+char* uni_strchug( char* in, char c )
+{
+	if(!in || !c)
+	{
+		uni_die( );
+	}
+
+	{
+		ptri i, offs;
+		const ptri in_sz = uni_strlen( in );
+
+		for(i = in_sz - 1, offs = 0; i >= 0; --i, ++offs)
+		{
+			if(in[i] != c)
+			{
+				break;
+			}
+		}
+
+		in[in_sz - offs] = '\0';
+
+		return in;
 	}
 }
