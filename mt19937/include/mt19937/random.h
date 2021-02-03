aee45f43bc57a3365303f1f4f54665593c492b0e
Alexander Nicholi <me@nicholatian.com>
2021-02-03 15:41:38 -0500

    reformat code

--- mt19937/include/mt19937/random.h
+++ mt19937/include/mt19937/random.h
@@ -18,21 +18,21 @@ extern "C" {
 
 struct prng;
 
-struct mt_prng *mt_prng_init(void);
-void mt_prng_fini(struct mt_prng *);
+struct mt_prng * mt_prng_init( void );
+void mt_prng_fini( struct mt_prng * );
 
-s32 mt_range_s32(struct mt_prng *, s32, s32);
-u32 mt_range_u32(struct mt_prng *, u32, u32);
-#if !defined(CFG_GBA)
-s64 mt_range_s64(struct mt_prng *, s64, s64);
-u64 mt_range_u64(struct mt_prng *, u64, u64);
+s32 mt_range_s32( struct mt_prng *, s32, s32 );
+u32 mt_range_u32( struct mt_prng *, u32, u32 );
+#if !defined( CFG_GBA )
+s64 mt_range_s64( struct mt_prng *, s64, s64 );
+u64 mt_range_u64( struct mt_prng *, u64, u64 );
 #endif
 
-s32 mt_random_s32(struct mt_prng *);
-u32 mt_random_u32(struct mt_prng *);
-#if !defined(CFG_GBA)
-s64 mt_random_s64(struct mt_prng *);
-u64 mt_random_u64(struct mt_prng *);
+s32 mt_random_s32( struct mt_prng * );
+u32 mt_random_u32( struct mt_prng * );
+#if !defined( CFG_GBA )
+s64 mt_random_s64( struct mt_prng * );
+u64 mt_random_u64( struct mt_prng * );
 #endif
 
 #ifdef __cplusplus
